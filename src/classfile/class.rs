d73031bf2a42d33ddb9c954c082d5228ab89f4df
Lord-McSweeney <Lord-McSweeney@github.com>
2024-10-23 14:20:36 -0700

    classfile: Read this- and super-class into ClassFile

--- src/classfile/class.rs
+++ src/classfile/class.rs
@@ -1,13 +1,18 @@
-use super::constant_pool::{read_constant_pool, ConstantPool};
+use super::constant_pool::{read_constant_pool, ConstantPool, ConstantPoolEntry};
 use super::error::Error;
 use super::flags::ClassFlags;
 use super::reader::{FileData, Reader};
+
 use crate::gc::GcCtx;
+use crate::string::JvmString;
 
 pub struct ClassFile {
     constant_pool: ConstantPool,
 
     flags: ClassFlags,
+
+    this_class_name: JvmString,
+    super_class_name: Option<JvmString>,
 }
 
 pub fn read_class(gc_ctx: &GcCtx, data: Vec<u8>) -> Result<ClassFile, Error> {
@@ -26,8 +31,51 @@ pub fn read_class(gc_ctx: &GcCtx, data: Vec<u8>) -> Result<ClassFile, Error> {
     let flag_bits = reader.read_u16()?;
     let flags = ClassFlags::from_bits_truncate(flag_bits);
 
+    // Read this-class name
+    let this_class_idx = reader.read_u16()?;
+    let this_class = constant_pool.entry(this_class_idx)?;
+
+    let this_class_name = match this_class {
+        ConstantPoolEntry::Class { name_idx } => {
+            let entry = constant_pool.entry(name_idx)?;
+
+            let ConstantPoolEntry::Utf8 { string } = entry else {
+                // Guaranteed by validation
+                unreachable!();
+            };
+
+            string
+        }
+        _ => return Err(Error::ConstantPoolTypeMismatch),
+    };
+
+    // Read superclass name
+    let super_class_idx = reader.read_u16()?;
+
+    let super_class_name = if super_class_idx == 0 {
+        None
+    } else {
+        let super_class = constant_pool.entry(super_class_idx)?;
+
+        Some(match super_class {
+            ConstantPoolEntry::Class { name_idx } => {
+                let entry = constant_pool.entry(name_idx)?;
+
+                let ConstantPoolEntry::Utf8 { string } = entry else {
+                    // Guaranteed by validation
+                    unreachable!();
+                };
+
+                string
+            }
+            _ => return Err(Error::ConstantPoolTypeMismatch),
+        })
+    };
+
     Ok(ClassFile {
         constant_pool,
         flags,
+        this_class_name,
+        super_class_name,
     })
 }
--- src/string/jvm_string.rs
+++ src/string/jvm_string.rs
@@ -2,12 +2,19 @@
 // stored as UTF-8 strings. However, the java/lang/String class stores strings
 // as UTF-16, so conversion is needed.
 
+use std::fmt;
 use std::ops::Deref;
 
 use crate::gc::{Gc, GcCtx};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct JvmString(Gc<String>);
+
+impl fmt::Display for JvmString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", &*self.0)
+    }
+}
 
 impl JvmString {
     pub fn new(gc_ctx: &GcCtx, string: String) -> Self {
