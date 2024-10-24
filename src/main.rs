d7ad231e306e4f3d2e9533cee6bc91170579e816
Lord-McSweeney <Lord-McSweeney@github.com>
2024-10-24 12:08:32 -0700

    main: Commit main
    Can't believe I forgot to do this

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+mod classfile;
+mod gc;
+mod runtime;
+mod string;
+
+use crate::classfile::class::ClassFile;
+use crate::gc::{Gc, GcCtx, Trace};
+use crate::runtime::class::Class;
+use crate::runtime::context::Context;
+
+use std::env;
+use std::fs;
+
+struct Root {}
+
+impl Trace for Root {
+    fn trace(&self) {}
+}
+
+fn main() {
+    let gc_ctx = GcCtx::new();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Run as {} [file.class]", args[0]);
+        return;
+    }
+
+    let read_file = match fs::read(&args[1]) {
+        Ok(data) => data,
+        Err(error) => {
+            println!("Error: {}", error.to_string());
+            return;
+        }
+    };
+
+    let context = Context::new(gc_ctx);
+
+    let class_file = ClassFile::from_data(context.gc_ctx, read_file).unwrap();
+    let main_class =
+        Class::from_class_file(context, class_file).expect("Failed to load main class");
+}
