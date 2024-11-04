c91ef1403782804c820243bf39d40993a3454e6a
Lord-McSweeney <Lord-McSweeney@github.com>
2024-11-04 12:13:48 -0800

    runtime: Add default value for Byte descriptor

--- src/runtime/descriptor.rs
+++ src/runtime/descriptor.rs
@@ -83,8 +83,9 @@ impl Descriptor {
     pub fn default_value(self) -> Value {
         match self {
             Descriptor::Class(_) | Descriptor::Array(_) => Value::Object(None),
-            Descriptor::Boolean => Value::Integer(0),
+            Descriptor::Byte => Value::Integer(0),
             Descriptor::Integer => Value::Integer(0),
+            Descriptor::Boolean => Value::Integer(0),
             _ => unimplemented!(),
         }
     }
