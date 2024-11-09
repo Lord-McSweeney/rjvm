e70d1e9762ade089daf7c596eb1b89bf1ebda0af
Lord-McSweeney <Lord-McSweeney@github.com>
2024-11-08 16:00:23 -0800

    globals: Add ArrayIndexOutOfBoundsException constructor with int parameter

--- src/runtime/globals/java/lang/ArrayIndexOutOfBoundsException.java
+++ src/runtime/globals/java/lang/ArrayIndexOutOfBoundsException.java
@@ -1,3 +1,12 @@
 package java.lang;
 
-public class ArrayIndexOutOfBoundsException extends IndexOutOfBoundsException { }
+public class ArrayIndexOutOfBoundsException extends IndexOutOfBoundsException {
+    public ArrayIndexOutOfBoundsException() {
+        super();
+    }
+
+    public ArrayIndexOutOfBoundsException(int index) {
+        // TODO implement
+        super();
+    }
+}
