e2d78e6c7de5659e957d0b26f575ad56cb12fefb
Lord-McSweeney <Lord-McSweeney@github.com>
2024-11-10 20:02:59 -0800

    globals: Add Enumeration interface and add abstract methods to Dictionary class

--- src/runtime/globals/java/util/Hashtable.java
+++ src/runtime/globals/java/util/Hashtable.java
@@ -1,3 +1,42 @@
 package java.util;
 
-public class Hashtable<K, V> extends Dictionary<K, V> implements Map<K, V> { }
+public class Hashtable<K, V> extends Dictionary<K, V> implements Map<K, V> {
+    public Hashtable() {
+        // TODO implement
+    }
+
+    public int size() {
+        // TODO implement
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO implement
+        return true;
+    }
+
+    public Enumeration<K> keys() {
+        // TODO implement
+        return null;
+    }
+
+    public Enumeration<V> elements() {
+        // TODO implement
+        return null;
+    }
+
+    public V get(Object key) {
+        // TODO implement
+        return null;
+    }
+
+    public V put(K key, V value) {
+        // TODO implement
+        return null;
+    }
+
+    public V remove(Object key) {
+        // TODO implement
+        return null;
+    }
+}
