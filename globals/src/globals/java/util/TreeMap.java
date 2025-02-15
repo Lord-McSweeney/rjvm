aec7f348424a6a8aad13d0aa9ed01ee8e09ba352
Lord-McSweeney <Lord-McSweeney@github.com>
2025-02-15 10:24:28 -0800

    globals: Add TreeMap.entrySet

--- globals/src/globals/java/util/TreeMap.java
+++ globals/src/globals/java/util/TreeMap.java
@@ -1,13 +1,21 @@
 package java.util;
 
 public class TreeMap<K, V> extends AbstractMap<K, V> {
+    Object[] keys;
+    Object[] values;
+
+    EntrySet cachedEntrySet;
+
     public TreeMap(Comparator<? super K> comparator) {
-        super();
+        this.keys = new Object[0];
+        this.values = new Object[0];
     }
 
     public Set<Entry<K, V>> entrySet() {
-        // TODO implement
-        return null;
+        if (this.cachedEntrySet == null) {
+            this.cachedEntrySet = new EntrySet();
+        }
+        return this.cachedEntrySet;
     }
 
     public K firstKey() {
@@ -33,5 +41,19 @@ public class TreeMap<K, V> extends AbstractMap<K, V> {
     public SortedMap<K, V> subMap(K lowestKey, K highestKey) {
         // TODO implement
         return null;
+    }
+
+    class EntrySet extends AbstractSet<Entry<K, V>> {
+        // TODO implement
+
+        public Iterator<Entry<K, V>> iterator() {
+            // TODO implement
+            return new ArrayIterator(new Object[0]);
+        }
+
+        public int size() {
+            // TODO implement
+            return 0;
+        }
     }
 }
