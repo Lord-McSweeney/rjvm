c185bcf1fbb47ab4c748fdef59c38c88fd4a3df3
Lord-McSweeney <Lord-McSweeney@github.com>
2025-02-15 22:53:10 -0800

    globals: Ensure FileInputStreams cannot be constructed on directories

--- globals/src/globals/java/io/FileInputStream.java
+++ globals/src/globals/java/io/FileInputStream.java
@@ -1,13 +1,27 @@
 package java.io;
 
 public class FileInputStream extends InputStream {
-    public FileInputStream(String fileName) {
+    private FileDescriptor fd;
+    private boolean isOpen;
+
+    public FileInputStream(String fileName) throws FileNotFoundException {
         this(new File(fileName));
     }
 
-    public FileInputStream(File file) {
-        // TODO implement
-        super();
+    public FileInputStream(File file) throws FileNotFoundException {
+        FileDescriptor fd = FileDescriptor.fromFile(file);
+
+        this.fd = fd;
+        this.isOpen = true;
+    }
+
+    public FileInputStream(FileDescriptor fd) {
+        this.fd = fd;
+        if (fd.valid()) {
+            this.isOpen = true;
+        } else {
+            this.isOpen = false;
+        }
     }
 
     public int read() throws IOException {
