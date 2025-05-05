51840b90c190882196fa3b104667021eeaef4666
Lord-McSweeney <Lord-McSweeney@github.com>
2025-05-04 17:58:26 -0700

    globals: Implement Enum.ordinal

--- globals/globals/java/lang/Enum.java
+++ globals/globals/java/lang/Enum.java
@@ -1,8 +1,15 @@
 package java.lang;
 
 public abstract class Enum<E extends Enum<E>> {
+    private int ordinal;
+
     protected Enum(String name, int ordinal) {
         super();
+        this.ordinal = ordinal;
+    }
+
+    public final int ordinal() {
+        return this.ordinal;
     }
 
     public static <T extends Enum<T>> T valueOf(Class<T> enumType, String name) {
