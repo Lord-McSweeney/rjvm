d6e4ea0e3d220c1b31adf12dbfeb8c317bb2c2ff
Lord-McSweeney <Lord-McSweeney@github.com>
2025-07-02 20:32:41 -0700

    globals: Implement HashMap.remove and HashMap.containsKey

--- globals/globals/java/util/HashMap.java
+++ globals/globals/java/util/HashMap.java
@@ -36,7 +36,19 @@ public class HashMap<K, V> extends AbstractMap<K, V> {
     }
 
     public boolean containsKey(K key) {
-        Todo.warnNotImpl("java.util.HashMap.containsKey");
+        for (int i = 0; i < this.keys.length; i ++) {
+            if (this.keys[i] == null) {
+                if (key == null) {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+
+            if (this.keys[i].equals(key)) {
+                return true;
+            }
+        }
 
         return false;
     }
@@ -99,9 +111,51 @@ public class HashMap<K, V> extends AbstractMap<K, V> {
     }
 
     public V remove(Object key) {
-        Todo.warnNotImpl("java.util.HashMap.remove");
+        int pos = -1;
+        Object previousValue = null;
 
-        return null;
+        for (int i = 0; i < this.keys.length; i ++) {
+            if (this.keys[i] == null) {
+                if (key == null) {
+                    // The key was present, so record the index
+                    pos = i;
+                    previousValue = this.values[i];
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            if (this.keys[i].equals(key)) {
+                pos = i;
+                previousValue = this.values[i];
+                break;
+            }
+        }
+
+        if (pos == -1) {
+            return null;
+        }
+        // The key wasn't present, so resize the arrays, insert the
+        // key and value at the end, and return null.
+        int newSize = this.size() - 1;
+        Object[] newKeys = new Object[newSize];
+        Object[] newValues = new Object[newSize];
+
+        System.arraycopy(this.keys, 0, newKeys, 0, pos);
+        if (pos != newSize) {
+            System.arraycopy(this.keys, pos + 1, newKeys, pos + 1, newSize - pos);
+        }
+
+        System.arraycopy(this.values, 0, newValues, 0, pos);
+        if (pos != newSize) {
+            System.arraycopy(this.values, pos + 1, newValues, pos + 1, newSize - pos);
+        }
+
+        this.keys = newKeys;
+        this.values = newValues;
+
+        return (V) previousValue;
     }
 
     public void putAll(Map<? extends K, ? extends V> map) {
