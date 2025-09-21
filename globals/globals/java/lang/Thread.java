9120cb9a6e56ff317fbb1a5a54811efa449cc37c
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-20 21:46:44 -0700

    globals: Implement `Thread.run` and stub out `Thread.start`

--- globals/globals/java/lang/Thread.java
+++ globals/globals/java/lang/Thread.java
@@ -1,7 +1,22 @@
 package java.lang;
 
 public class Thread implements Runnable {
+    private Runnable target;
+
     public Thread() { }
 
-    public void run() { }
+    public Thread(Runnable target) {
+        this.target = target;
+    }
+
+    public synchronized void start() {
+        // TODO: Call the `run` method from a different thread
+        this.run();
+    }
+
+    public void run() {
+        if (this.target != null) {
+            this.target.run();
+        }
+    }
 }
