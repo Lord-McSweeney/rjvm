0ec82f3ae99ab5138b309cd4505a8fc6d39b34b3
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-24 10:50:51 -0700

    tests: Add a test for basic ArrayList functions

---
+++ tests/tests/array_list_basic/Test.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(6);
+        list.add(3);
+        list.add(-652);
+        list.add(289);
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(4);
+        System.out.println(list);
+        System.out.println(list.size());
+        list.add(0, 19);
+        list.add(list.size(), 2);
+        System.out.println(list);
+        System.out.println(list.size());
+        for (int i = 0; i < list.size(); i ++) {
+            System.out.println(list.get(i));
+        }
+        try {
+            System.out.println(list.get(-1));
+        } catch(RuntimeException e) {
+            System.out.println("Caught exception");
+        }
+        try {
+            System.out.println(list.get(list.size()));
+        } catch(RuntimeException e) {
+            System.out.println("Caught exception");
+        }
+        for (int elem : list) {
+            System.out.println(elem);
+        }
+    }
+}
