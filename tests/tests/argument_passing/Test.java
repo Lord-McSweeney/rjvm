6f7870373000bb59750cfea2387a5ccb915cd6d9
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-25 14:51:40 -0700

    tests: Add test for passing all kinda of numerical arguments

---
+++ tests/tests/argument_passing/Test.java
@@ -0,0 +1,41 @@
+public class Test {
+    public static void main(String[] args) {
+        Test.func(
+            1453252,
+            (byte) 5,
+            (short) -92,
+            (char) 28,
+            true,
+            74827348237L,
+            3899832.1,
+            5732.75f,
+            1453452,
+            (byte) 4,
+            (short) -93,
+            (char) 29,
+            false,
+            74837358237L,
+            3919832.1,
+            5732.25f
+        );
+    }
+
+    public static void func(int a1, byte a2, short a3, char a4, boolean a5, long a6, double a7, float a8, int a9, byte a10, short a11, char a12, boolean a13, long a14, double a15, float a16) {
+        System.out.println((int) a1);
+        System.out.println((int) a2);
+        System.out.println((int) a3);
+        System.out.println((int) a4);
+        System.out.println(a5);
+        System.out.println((int) a6);
+        System.out.println((int) a7);
+        System.out.println((int) a8);
+        System.out.println((int) a9);
+        System.out.println((int) a10);
+        System.out.println((int) a11);
+        System.out.println((int) a12);
+        System.out.println(a13);
+        System.out.println((int) a14);
+        System.out.println((int) a15);
+        System.out.println((int) a16);
+    }
+}
