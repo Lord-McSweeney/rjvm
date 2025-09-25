e9f4a13cb05d09024fb65f98a27573fdf19fc08e
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-24 18:52:05 -0700

    tests: Add test for iushr

--- tests/tests/int_ops/Test.java
+++ tests/tests/int_ops/Test.java
@@ -5,6 +5,8 @@ public class Test {
         int v2 = 9;
         int v3 = 1;
         int v4 = 4;
+        int v5 = 9;
+        int v6 = -3;
 
         System.out.println(v1 + v2);
         System.out.println(v2 - v3);
@@ -21,6 +23,7 @@ public class Test {
         System.out.println(~v2);
         System.out.println(v3 ^ v1);
         System.out.println(v2 ^ v4);
+        System.out.println(v6 >>> v3);
         try {
             System.out.println(v4 / v0);
         } catch(ArithmeticException e) {
@@ -30,6 +33,31 @@ public class Test {
             System.out.println(v3 % v0);
         } catch(ArithmeticException e) {
             System.out.println("Modulo by 0 threw exception");
+        }
+
+        System.out.println(v0 > v1);
+        System.out.println(v0 == v1);
+        System.out.println(v0 < v1);
+        System.out.println(v2 > v3);
+        System.out.println(v2 == v5);
+        System.out.println(v2 < v3);
+        if (v2 < v3) {
+            System.out.println("v2 < v3");
+        }
+        if (v4 > v5) {
+            System.out.println("v4 > v5");
+        }
+        if (v2 > v3) {
+            System.out.println("v2 > v3");
+        }
+        if (v0 > v1) {
+            System.out.println("v0 > v1");
+        }
+        if (v2 == v5) {
+            System.out.println("v2 == v5");
+        }
+        if (v0 == v1) {
+            System.out.println("v0 == v1");
         }
     }
 }
