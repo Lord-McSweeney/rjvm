3973308e0a29fb3e42fa67fff0ddbe5ecdf6c3bb
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-25 23:15:37 -0700

    tests: Add test for `Math.pow`

---
+++ tests/tests/math_pow/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println((int) Math.pow(3, 0));
+        System.out.println((int) Math.pow(338.72823, 32878.213));
+    }
+}
