a06d402c4e7e6743900a4b60c39501329e559e49
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-26 20:50:31 -0700

    tests: Add `abs` and `signum` to `math_funcs` test

--- tests/tests/math_funcs/Test.java
+++ tests/tests/math_funcs/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println((int) Math.abs(3.0));
+        System.out.println((int) Math.abs(-1789.0));
+        System.out.println((int) Math.abs((-0.0) / 1.0));
+        System.out.println((int) Math.abs(234774832.278));
+        System.out.println((int) Math.abs(-0.778));
+        System.out.println((int) Math.abs(0.0));
+
         System.out.println((int) (Math.atan2(3, 0) * 1000));
         System.out.println((int) (Math.atan2(338.72823, 32878.213) * 1000));
         System.out.println((int) (Math.atan2(3.72823, 78.213) * 1000));
@@ -12,20 +19,27 @@ public class Test {
         System.out.println((int) (Math.atan2(0, 0) * 1000));
         System.out.println((int) (Math.atan2(0, -2) * 1000));
 
-        System.out.println((int) Math.pow(3, 0));
-        System.out.println((int) Math.pow(338.72823, 32878.213));
-
-        System.out.println((int) Math.sqrt(3.0));
-        System.out.println((int) Math.sqrt(1789));
-        System.out.println((int) Math.sqrt(400));
-        System.out.println((int) Math.sqrt(234774832.278));
-        System.out.println((int) Math.sqrt(0.778));
-
         System.out.println((int) Math.log(2));
         System.out.println((int) Math.log(3.0));
         System.out.println((int) Math.log(1789));
         System.out.println((int) Math.log(400));
         System.out.println((int) Math.log(234774832.278));
         System.out.println((int) Math.log(0.778));
+
+        System.out.println((int) Math.pow(3, 0));
+        System.out.println((int) Math.pow(338.72823, 32878.213));
+
+        System.out.println((int) Math.signum(3.0));
+        System.out.println((int) Math.signum(-1789.0));
+        System.out.println((int) Math.signum((-0.0) / 1.0));
+        System.out.println((int) Math.signum(234774832.278));
+        System.out.println((int) Math.signum(-0.778));
+        System.out.println((int) Math.signum(0.0));
+
+        System.out.println((int) Math.sqrt(3.0));
+        System.out.println((int) Math.sqrt(1789));
+        System.out.println((int) Math.sqrt(400));
+        System.out.println((int) Math.sqrt(234774832.278));
+        System.out.println((int) Math.sqrt(0.778));
     }
 }
