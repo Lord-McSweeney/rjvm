5d0fb35f36ad91e6dc6bc30f63005e484cde2932
Lord-McSweeney <batuharmanci@gmail.com>
2025-09-26 20:50:11 -0700

    globals: Implement `Math.signum` and `Math.abs`

--- globals/globals/base/java/lang/Math.java
+++ globals/globals/base/java/lang/Math.java
@@ -1,11 +1,37 @@
 package java.lang;
 
 public class Math {
+    public static double abs(double v) {
+        // FIXME are we handling negative zero correctly?
+        if (v < 0) {
+            return -v;
+        } else {
+            return v;
+        }
+    }
+
     public static native double atan2(double y, double x);
 
     public static native double log(double v);
 
     public static native double pow(double b, double e);
+
+    public static double signum(double v) {
+        if (v == v) {
+            // not-NaN
+            if (v == 0) {
+                // `return v` handles -0 correctly
+                return v;
+            } else if (v < 0) {
+                return -1.0;
+            } else {
+                return 1.0;
+            }
+        } else {
+            // NaN
+            return v;
+        }
+    }
 
     public static native double sqrt(double v);
 }
