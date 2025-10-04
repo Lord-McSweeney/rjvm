755abf60e8291f8fc3f5737c10771b76f554097f
Lord-McSweeney <batuharmanci@gmail.com>
2025-10-04 14:53:11 -0700

    globals: Implement `BitSet.get`

--- globals/globals/base/java/util/BitSet.java
+++ globals/globals/base/java/util/BitSet.java
@@ -1,18 +1,28 @@
 package java.util;
 
-public class BitSet {
+public class BitSet implements Cloneable {
     // This is inefficient (each byte stores a single bit), but it's good enough for now
-    private byte[] bits;
+    private boolean[] bits;
 
     public BitSet() {
-        this.bits = new byte[0];
+        this.bits = new boolean[0];
     }
 
     public BitSet(int numBits) {
-        this.bits = new byte[numBits];
+        this.bits = new boolean[numBits];
     }
 
     public void set(int index) {
-        // TODO implement
+        // TODO auto-expand
+        this.bits[index] = true;
+    }
+
+    public boolean get(int index) {
+        // This does not auto-expand
+        if (index < this.bits.length) {
+            return this.bits[index];
+        } else {
+            return false;
+        }
     }
 }
