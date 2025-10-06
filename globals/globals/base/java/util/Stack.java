377d61b6f474c1c4c41208e546397ba00e386b4d
Lord-McSweeney <batuharmanci@gmail.com>
2025-10-06 08:44:16 -0700

    globals: Implement some Vector and Stack functions

--- globals/globals/base/java/util/Stack.java
+++ globals/globals/base/java/util/Stack.java
@@ -1,3 +1,14 @@
 package java.util;
 
-public class Stack<E> extends Vector<E> { }
+public class Stack<E> extends Vector<E> {
+    public E pop() {
+        if (this.size() == 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return this.remove(this.size() - 1);
+    }
+
+    public boolean empty() {
+        return this.size() == 0;
+    }
+}
--- globals/globals/base/java/util/Vector.java
+++ globals/globals/base/java/util/Vector.java
@@ -1,8 +1,10 @@
 package java.util;
 
 public class Vector<E> extends AbstractList<E> {
+    Object[] data;
+
     public Vector() {
-        super();
+        this.data = new Object[0];
     }
 
     public boolean add(E element) {
@@ -11,31 +13,58 @@ public class Vector<E> extends AbstractList<E> {
     }
 
     public void add(int index, E element) {
-        // TODO implement
+        Object[] newData = new Object[this.data.length + 1];
+
+        System.arraycopy(this.data, 0, newData, 0, index);
+        newData[index] = element;
+        System.arraycopy(this.data, index, newData, index + 1, this.size() - index);
+
+        this.data = newData;
     }
 
     public E get(int index) {
-        // TODO implement
-        return null;
+        if (index < 0 || index >= this.data.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return (E) this.data[index];
     }
 
     public E set(int index, E element) {
-        // TODO implement
-        return null;
+        if (index < 0 || index >= this.data.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E oldElement = (E) this.data[index];
+
+        this.data[index] = element;
+
+        return oldElement;
     }
 
     public E remove(int index) {
-        // TODO implement
-        return null;
+        if (index < 0 || index >= this.data.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E oldElement = (E) this.data[index];
+
+        Object[] newData = new Object[this.data.length - 1];
+
+        System.arraycopy(this.data, 0, newData, 0, index);
+        System.arraycopy(this.data, index + 1, newData, index, this.size() - index - 1);
+
+        this.data = newData;
+
+        return oldElement;
     }
 
     public void clear() {
-        // TODO implement
+        this.data = new Object[0];
     }
 
     public int size() {
-        // TODO implement
-        return 0;
+        return this.data.length;
     }
 
     public boolean isEmpty() {
