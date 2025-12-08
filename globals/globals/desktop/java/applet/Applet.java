ad2c3aafa92a0888620be160898b947712d59354
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-08 15:01:19 -0800

    globals: Add `Applet`, `Panel`, and `Window` classes

---
+++ globals/globals/desktop/java/applet/Applet.java
@@ -0,0 +1,21 @@
+package java.applet;
+
+import java.awt.Panel;
+
+public class Applet extends Panel {
+    public String getParameter(String name) {
+        return null;
+    }
+
+    public void init() {
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+    public void destroy() {
+    }
+}
