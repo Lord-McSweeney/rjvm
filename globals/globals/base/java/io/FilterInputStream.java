666fde6e7649d9298373223390e7b456d519c705
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-09 20:11:26 -0800

    globals: Improve `FilterInputStream` support

--- globals/globals/base/java/io/FilterInputStream.java
+++ globals/globals/base/java/io/FilterInputStream.java
@@ -14,4 +14,16 @@ public class FilterInputStream extends InputStream {
     public int read() throws IOException {
         return this.in.read();
     }
+
+    public int read(byte buffer[]) throws IOException {
+        return this.read(buffer, 0, buffer.length);
+    }
+
+    public int read(byte buffer[], int offset, int length) throws IOException {
+        return this.in.read(buffer, offset, length);
+    }
+
+    public void close() throws IOException {
+        this.in.close();
+    }
 }
