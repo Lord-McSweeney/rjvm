c1d2fc20e07a312c33471caf910f91c101149432
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-10 15:14:52 -0800

    globals: Add `java.awt.MediaTracker` class

---
+++ globals/globals/desktop/java/awt/MediaTracker.java
@@ -0,0 +1,17 @@
+package java.awt;
+
+public class MediaTracker {
+    private Component comp;
+
+    public MediaTracker(Component comp) {
+        this.comp = comp;
+    }
+
+    public void addImage(Image image, int id) {
+        // TODO implement
+    }
+
+    public void waitForID(int id) throws InterruptedException {
+        // TODO implement
+    }
+}
