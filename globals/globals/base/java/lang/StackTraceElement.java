708e64e45ed5fcf6e5183385a1ddee27347438b9
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-09 16:53:58 -0800

    globals: Add `java.lang.StackTraceElement` class

---
+++ globals/globals/base/java/lang/StackTraceElement.java
@@ -0,0 +1,47 @@
+package java.lang;
+
+public final class StackTraceElement {
+    private String declaringClass;
+    private String methodName;
+    private String fileName;
+    private int lineNumber;
+    private boolean isNativeMethod;
+
+    public StackTraceElement(String declaringClass, String methodName, String fileName, int lineNumber) {
+        if (declaringClass == null || methodName == null) {
+            throw new NullPointerException();
+        }
+
+        this.declaringClass = declaringClass;
+        this.methodName = methodName;
+        this.fileName = fileName;
+        this.lineNumber = lineNumber;
+
+        // TODO
+        this.isNativeMethod = false;
+    }
+
+    public String getClassName() {
+        return this.declaringClass;
+    }
+
+    public String getMethodName() {
+        return this.methodName;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public int getLineNumber() {
+        return this.lineNumber;
+    }
+
+    public boolean isNativeMethod() {
+        return this.isNativeMethod;
+    }
+
+    public String toString() {
+        return this.declaringClass + '.' + this.methodName + "()";
+    }
+}
