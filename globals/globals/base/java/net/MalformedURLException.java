767976bb4ef40754d40c22db3b86b6a2d074d86f
Lord-McSweeney <batuharmanci@gmail.com>
2025-12-11 11:55:53 -0800

    globals: Add `flash.net.URL` class stub

---
+++ globals/globals/base/java/net/MalformedURLException.java
@@ -0,0 +1,9 @@
+package java.net;
+
+import java.io.IOException;
+
+public class MalformedURLException extends IOException {
+    public MalformedURLException() {
+        super();
+    }
+}
---
+++ globals/globals/base/java/net/URL.java
@@ -0,0 +1,11 @@
+package java.net;
+
+public final class URL {
+    public URL​(String spec) throws MalformedURLException {
+        // TODO
+    }
+
+    public URL​(URL context, String spec) throws MalformedURLException {
+        // TODO
+    }
+}
