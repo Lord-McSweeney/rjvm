5781447e1afcafcf957d1d1eac7f7cca2dab1b5f
Lord-McSweeney <batuharmanci@gmail.com>
2026-01-24 08:40:58 -0800

    globals: Add some more `java.lang.invoke` classes
    - `LambdaConversionException`
    - `LambdaMetafactory`
    - `MethodHandles`

---
+++ globals/globals/base/java/lang/invoke/LambdaConversionException.java
@@ -0,0 +1,3 @@
+package java.lang.invoke;
+
+public class LambdaConversionException extends Exception { }
---
+++ globals/globals/base/java/lang/invoke/LambdaMetafactory.java
@@ -0,0 +1,8 @@
+package java.lang.invoke;
+
+public class LambdaMetafactory {
+    public static CallSite metafactory(MethodHandles.Lookup caller, String invokedName, MethodType invokedType, MethodType samMethodType, MethodHandle implMethod, MethodType instantiatedMethodType) throws LambdaConversionException {
+        // TODO
+        return null;
+    }
+}
