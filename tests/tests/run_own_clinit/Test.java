b26186196fce82b98a14bf288c79fcdd59502a5b
Lord-McSweeney <batuharmanci@gmail.com>
2026-01-30 10:15:06 -0800

    tests: Add test for running clinit of owner class of method before running method

---
+++ tests/tests/run_own_clinit/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static {
+        System.out.println("Ran <clinit>");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ran main");
+    }
+}
