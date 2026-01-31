81a1c376ecbaca26f92b4f91bb8617ada9221ad5
Lord-McSweeney <batuharmanci@gmail.com>
2026-01-30 18:11:16 -0800

    globals: Implement basic use of throwable causes

--- globals/globals/base/java/lang/Throwable.java
+++ globals/globals/base/java/lang/Throwable.java
@@ -3,18 +3,38 @@ package java.lang;
 import java.io.PrintStream;
 
 public class Throwable {
+    // NOTE field ordering is important
     private String message;
 
-    // NOTE field ordering is important
     private StackTraceElement[] stackTrace;
 
+    // If cause is equal to self, then it's not-yet-set
+    private Throwable cause;
+
+    // Constructors
     public Throwable() {
         this.fillInStackTrace();
+
+        // Make cause not-yet-set
+        this.cause = this;
     }
 
     public Throwable(String message) {
         this.fillInStackTrace();
         this.message = message;
+
+        // Make cause not-yet-set
+        this.cause = this;
+    }
+
+    public Throwable(Throwable cause) {
+        this.fillInStackTrace();
+
+        if (cause != null) {
+            this.message = cause.toString();
+        }
+
+        this.cause = cause;
     }
 
     public Throwable fillInStackTrace() {
@@ -23,6 +43,27 @@ public class Throwable {
     }
 
     private native StackTraceElement[] internalFillInStackTrace();
+
+    public synchronized Throwable getCause() {
+        if (this.cause == this) {
+            return null;
+        } else {
+            return this.cause;
+        }
+    }
+
+    public synchronized Throwable initCause(Throwable cause) {
+        if (this.cause != this) {
+            // Cause was already set
+            throw new IllegalStateException();
+        } else if (cause == this) {
+            // Cannot have cause set to self
+            throw new IllegalArgumentException();
+        } else {
+            this.cause = cause;
+            return this;
+        }
+    }
 
     public String getMessage() {
         return this.message;
