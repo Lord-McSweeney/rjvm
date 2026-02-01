36ccde4fa2430b6d603ec74ea75e21bf02b905d1
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-01 14:20:24 -0800

    globals: Stub out `Thread.currentThread()` and `Thread.getContextClassLoader()`

--- globals/globals/base/java/lang/Thread.java
+++ globals/globals/base/java/lang/Thread.java
@@ -2,11 +2,19 @@ package java.lang;
 
 public class Thread implements Runnable {
     private Runnable target;
+    private ClassLoader contextClassLoader;
 
-    public Thread() { }
+    public Thread() {
+        this.init(null);
+    }
 
     public Thread(Runnable target) {
+        this.init(target);
+    }
+
+    private void init(Runnable target) {
         this.target = target;
+        this.contextClassLoader = ClassLoader.getSystemClassLoader(); // TODO
     }
 
     public synchronized void start() {
@@ -20,7 +28,21 @@ public class Thread implements Runnable {
         }
     }
 
+    public ClassLoader getContextClassLoader() {
+        return this.contextClassLoader;
+    }
+
     public static void sleep(long millis) throws InterruptedException {
         // TODO implement
+    }
+
+    // :P
+    private static Thread currentThread;
+    public static Thread currentThread() {
+        if (Thread.currentThread == null) {
+            Thread.currentThread = new Thread();
+        }
+
+        return Thread.currentThread;
     }
 }
