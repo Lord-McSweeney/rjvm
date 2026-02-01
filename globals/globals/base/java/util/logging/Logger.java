3d8475b3d2c5cf43529d639bc2f95af3dbaa8a4c
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-01 13:33:55 -0800

    globals: Add `java.util.logging.Logger` class

---
+++ globals/globals/base/java/util/logging/Logger.java
@@ -0,0 +1,11 @@
+package java.util.logging;
+
+public class Logger {
+    protected Logger(String name, String resourceBundleName) {
+    }
+
+    public static Logger getLogger(String name) {
+        // TODO implement
+        return null;
+    }
+}
