d257917f2f65ef9894aa9d25e332cf1d55eef831
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-03 13:30:10 -0800

    globals: Implement `EnumSet` support, add functions to `Enum` class

--- globals/globals/base/java/lang/Enum.java
+++ globals/globals/base/java/lang/Enum.java
@@ -3,15 +3,29 @@ package java.lang;
 import rjvm.internal.Todo;
 
 public abstract class Enum<E extends Enum<E>> {
+    private String name;
     private int ordinal;
 
     protected Enum(String name, int ordinal) {
         super();
+        this.name = name;
         this.ordinal = ordinal;
+    }
+
+    public final String name() {
+        return this.name;
     }
 
     public final int ordinal() {
         return this.ordinal;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public final boolean equals(Object o) {
+        return this == o;
     }
 
     public static <T extends Enum<T>> T valueOf(Class<T> enumType, String name) {
--- globals/globals/base/java/util/EnumSet.java
+++ globals/globals/base/java/util/EnumSet.java
@@ -1,11 +1,92 @@
 package java.util;
 
-import rjvm.internal.Todo;
-
 public abstract class EnumSet<E extends Enum<E>> extends AbstractSet<E> {
-    public static <E extends Enum<E>> EnumSet<E> allOf(Class<E> elementType) {
-        Todo.warnNotImpl("java.util.EnumSet.allOf");
+    public static <E extends Enum<E>> EnumSet<E> noneOf(Class<E> elementType) {
+        return new ConcreteEnumSet(elementType);
+    }
 
-        return null;
+    public static <E extends Enum<E>> EnumSet<E> allOf(Class<E> elementType) {
+        ConcreteEnumSet<E> result = (ConcreteEnumSet<E>) EnumSet.noneOf(elementType);
+
+        result.addAllVariants();
+
+        return result;
+    }
+}
+
+class ConcreteEnumSet<E extends Enum<E>> extends EnumSet<E> {
+    // The array of current variants. Variants that aren't present in the set
+    // are marked by a `null`.
+    Object[] variants;
+
+    // All possible variants.
+    Object[] allVariants;
+
+    ConcreteEnumSet(Class<E> elementType) {
+        this.allVariants = elementType.getEnumConstants();
+        this.variants = new Object[allVariants.length];
+    }
+
+    void addAllVariants() {
+        this.variants = this.allVariants.clone();
+    }
+
+    public int size() {
+        int size = 0;
+
+        for (int i = 0; i < this.variants.length; i ++) {
+            if (this.variants[i] != null) {
+                size += 1;
+            }
+        }
+
+        return size;
+    }
+
+    public Iterator<E> iterator() {
+        return new ConcreteEnumSetIterator<E>(this);
+    }
+}
+
+final class ConcreteEnumSetIterator<E extends Enum<E>> implements Iterator<E> {
+    private int nextIndex;
+    private ConcreteEnumSet<E> set;
+
+    public ConcreteEnumSetIterator(ConcreteEnumSet<E> set) {
+        this.nextIndex = 0;
+        this.set = set;
+
+        this.recalculateNext();
+    }
+
+    private void recalculateNext() {
+        while (true) {
+            if (this.nextIndex == this.set.variants.length) {
+                // At the end
+                return;
+            }
+
+            if (this.set.variants[this.nextIndex] != null) {
+                // Found the next one
+                return;
+            }
+
+            this.nextIndex += 1;
+        }
+    }
+
+    public boolean hasNext() {
+        return this.nextIndex != this.set.variants.length;
+    }
+
+    public E next() {
+        if (!this.hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        E result = (E) this.set.variants[this.nextIndex];
+        this.nextIndex += 1;
+        this.recalculateNext();
+        return result;
     }
 }
