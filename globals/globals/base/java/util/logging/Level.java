0ae370858567d70c77c07e6290001e64e6bb1cd2
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-03 20:08:08 -0800

    globals: Implement more methods and fields of `Level` and `Logger`

--- globals/globals/base/java/util/logging/Level.java
+++ globals/globals/base/java/util/logging/Level.java
@@ -1,4 +1,30 @@
 package java.util.logging;
 
 public class Level {
+    public static final Level OFF = new Level("OFF", Integer.MAX_VALUE, null);
+    public static final Level SEVERE = new Level("SEVERE", 1000, null);
+    public static final Level WARNING = new Level("WARNING", 900, null);
+    public static final Level INFO = new Level("INFO", 800, null);
+    public static final Level CONFIG = new Level("CONFIG", 700, null);
+    public static final Level FINE = new Level("FINE", 500, null);
+    public static final Level FINER = new Level("FINER", 400, null);
+    public static final Level FINEST = new Level("FINEST", 300, null);
+    public static final Level ALL = new Level("ALL", Integer.MIN_VALUE, null);
+
+    private final String name;
+
+    private final int value;
+
+    protected Level(String name, int value) {
+        this(name, value, null);
+    }
+
+    protected Level(String name, int value, String resourceBundleName) {
+        if (name == null) {
+            throw new NullPointerException();
+        }
+
+        this.name = name;
+        this.value = value;
+    }
 }
--- globals/globals/base/java/util/logging/Logger.java
+++ globals/globals/base/java/util/logging/Logger.java
@@ -4,8 +4,40 @@ public class Logger {
     protected Logger(String name, String resourceBundleName) {
     }
 
-    public static Logger getLogger(String name) {
+    public void entering(String sourceClass, String sourceMethod) {
         // TODO implement
-        return null;
+    }
+
+    public void entering(String sourceClass, String sourceMethod, Object param1) {
+        // TODO implement
+    }
+
+    public void entering(String sourceClass, String sourceMethod, Object[] params) {
+        // TODO implement
+    }
+
+    public void log(Level level, String msg) {
+        // TODO implement
+    }
+
+    public void log(Level level, String msg, Object param1) {
+        // TODO implement
+    }
+
+    public void log(Level level, String msg, Object[] params) {
+        // TODO implement
+    }
+
+    public void exiting(String sourceClass, String sourceMethod) {
+        // TODO implement
+    }
+
+    public void exiting(String sourceClass, String sourceMethod, Object result) {
+        // TODO implement
+    }
+
+    public static Logger getLogger(String name) {
+        // TODO implement properly
+        return new Logger(name, null);
     }
 }
