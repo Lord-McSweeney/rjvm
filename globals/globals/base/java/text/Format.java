16f91de1bb15d0c82de4f049fbf9808256b35166
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-07 22:18:52 -0800

    globals: Add `java.text.MessageFormat` class

---
+++ globals/globals/base/java/text/Format.java
@@ -0,0 +1,3 @@
+package java.text;
+
+public abstract class Format { }
---
+++ globals/globals/base/java/text/MessageFormat.java
@@ -0,0 +1,8 @@
+package java.text;
+
+public class MessageFormat extends Format {
+    public static String format(String pattern, Object... arguments) {
+        // TODO implement
+        return pattern;
+    }
+}
