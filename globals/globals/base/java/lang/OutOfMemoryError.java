05b7f2fe43b605077608b688b5e911d21edd6df6
Lord-McSweeney <batuharmanci@gmail.com>
2026-02-07 18:19:17 -0800

    globals: Implement various `VirtualMachineError`s

---
+++ globals/globals/base/java/lang/OutOfMemoryError.java
@@ -0,0 +1,7 @@
+package java.lang;
+
+public class OutOfMemoryError extends VirtualMachineError {
+    public OutOfMemoryError() {
+        super();
+    }
+}
---
+++ globals/globals/base/java/lang/VirtualMachineError.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+public abstract class VirtualMachineError extends Error {
+    public VirtualMachineError() {
+        super();
+    }
+
+    public VirtualMachineError(String message) {
+        super(message);
+    }
+}
