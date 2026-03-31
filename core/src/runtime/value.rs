243067855e1eda405b1301c78b7c85822fa25bba
Lord-McSweeney <batuharmanci@gmail.com>
2026-03-31 08:41:15 -0700

    runtime: Make enum variants of `Value` an implementation detail

--- core/src/runtime/value.rs
+++ core/src/runtime/value.rs
@@ -2,8 +2,14 @@ use super::object::Object;
 
 use crate::gc::Trace;
 
-#[derive(Clone, Copy, Debug)]
-pub enum Value {
+use core::fmt;
+
+#[derive(Clone, Copy)]
+#[repr(transparent)]
+pub struct Value(ValueData);
+
+#[derive(Clone, Copy)]
+enum ValueData {
     Reference(Option<Object>),
     Primitive(u64),
 }
@@ -12,62 +18,62 @@ impl Value {
     // Make Value from inner value
     #[allow(non_snake_case)]
     pub fn Integer(value: i32) -> Self {
-        Self::Primitive(value as u64)
+        Self(ValueData::Primitive(value as u64))
     }
 
     #[allow(non_snake_case)]
     pub fn Long(value: i64) -> Self {
-        Self::Primitive(value as u64)
+        Self(ValueData::Primitive(value as u64))
     }
 
     #[allow(non_snake_case)]
     pub fn Float(value: f32) -> Self {
-        Self::Primitive(f32::to_bits(value) as u64)
+        Self(ValueData::Primitive(f32::to_bits(value) as u64))
     }
 
     #[allow(non_snake_case)]
     pub fn Double(value: f64) -> Self {
-        Self::Primitive(f64::to_bits(value) as u64)
+        Self(ValueData::Primitive(f64::to_bits(value) as u64))
     }
 
     #[allow(non_snake_case)]
     pub fn Object(object: Option<Object>) -> Self {
-        Self::Reference(object)
+        Self(ValueData::Reference(object))
     }
 
     // Get inner value from Value
     pub fn int(self) -> i32 {
-        match self {
-            Value::Primitive(p) => p as i32,
-            Value::Reference(_) => panic!("Expected value to be primitive"),
+        match self.0 {
+            ValueData::Primitive(p) => p as i32,
+            ValueData::Reference(_) => panic!("Expected value to be primitive"),
         }
     }
 
     pub fn long(self) -> i64 {
-        match self {
-            Value::Primitive(p) => p as i64,
-            Value::Reference(_) => panic!("Expected value to be primitive"),
+        match self.0 {
+            ValueData::Primitive(p) => p as i64,
+            ValueData::Reference(_) => panic!("Expected value to be primitive"),
         }
     }
 
     pub fn float(self) -> f32 {
-        match self {
-            Value::Primitive(p) => f32::from_bits(p as u32),
-            Value::Reference(_) => panic!("Expected value to be primitive"),
+        match self.0 {
+            ValueData::Primitive(p) => f32::from_bits(p as u32),
+            ValueData::Reference(_) => panic!("Expected value to be primitive"),
         }
     }
 
     pub fn double(self) -> f64 {
-        match self {
-            Value::Primitive(p) => f64::from_bits(p),
-            Value::Reference(_) => panic!("Expected value to be primitive"),
+        match self.0 {
+            ValueData::Primitive(p) => f64::from_bits(p),
+            ValueData::Reference(_) => panic!("Expected value to be primitive"),
         }
     }
 
     pub fn object(self) -> Option<Object> {
-        match self {
-            Value::Reference(object) => object,
-            Value::Primitive(_) => panic!("Expected value to be object"),
+        match self.0 {
+            ValueData::Reference(object) => object,
+            ValueData::Primitive(_) => panic!("Expected value to be object"),
         }
     }
 }
@@ -77,9 +83,18 @@ const _: () = assert!(core::mem::size_of::<Value>() <= 16);
 impl Trace for Value {
     #[inline(always)]
     fn trace(&self) {
-        match self {
-            Value::Reference(object) => object.trace(),
+        match self.0 {
+            ValueData::Reference(object) => object.trace(),
             _ => {}
+        }
+    }
+}
+
+impl fmt::Debug for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self.0 {
+            ValueData::Primitive(p) => write!(f, "Value({})", p),
+            ValueData::Reference(o) => write!(f, "Value({:?})", o),
         }
     }
 }
