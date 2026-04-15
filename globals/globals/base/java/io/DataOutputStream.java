d6e5521c1f32c56bc5b6b410ab446dd6eee0d49f
Lord-McSweeney <batuharmanci@gmail.com>
2026-04-15 09:02:54 -0700

    globals: Implement DataOutputStream.writeLong

--- globals/globals/base/java/io/DataOutputStream.java
+++ globals/globals/base/java/io/DataOutputStream.java
@@ -33,16 +33,38 @@ public class DataOutputStream extends FilterOutputStream {
         int first =  (v >>> 24) & 0xFF;
         int second = (v >>> 16) & 0xFF;
         int third =  (v >>> 8) & 0xFF;
-        int last =   (v >>> 0) & 0xFF;
+        int fourth = (v >>> 0) & 0xFF;
 
         this.out.write(first);
         this.out.write(second);
         this.out.write(third);
-        this.out.write(last);
+        this.out.write(fourth);
 
         this.written += 4;
     }
-    
+
+    public final void writeLong(long v) throws IOException {
+        int first =   (int) ((v >>> 56) & 0xFF);
+        int second =  (int) ((v >>> 48) & 0xFF);
+        int third =   (int) ((v >>> 40) & 0xFF);
+        int fourth =  (int) ((v >>> 32) & 0xFF);
+        int fifth =   (int) ((v >>> 24) & 0xFF);
+        int sixth =   (int) ((v >>> 16) & 0xFF);
+        int seventh = (int) ((v >>> 8) & 0xFF);
+        int eighth =  (int) ((v >>> 0) & 0xFF);
+
+        this.out.write(first);
+        this.out.write(second);
+        this.out.write(third);
+        this.out.write(fourth);
+        this.out.write(fifth);
+        this.out.write(sixth);
+        this.out.write(seventh);
+        this.out.write(eighth);
+
+        this.written += 8;
+    }
+
     public final void writeUTF(String str) throws IOException {
         // TODO encode properly
 
