4c355f29c0ae356d31cf77b8d6b095c5c4ba8682
Lord-McSweeney <batuharmanci@gmail.com>
2026-05-12 18:34:38 -0700

    globals: Add a few more ZipFile stubs

--- globals/globals/base/java/util/zip/ZipFile.java
+++ globals/globals/base/java/util/zip/ZipFile.java
@@ -1,12 +1,25 @@
 package java.util.zip;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
 
-public class ZipFile {
+public class ZipFile implements Closeable {
+    private File file;
+
     public ZipFile(File file) {
+        this.file = file;
     }
 
     public ZipFile(String fileName) {
         this(new File(fileName));
+    }
+
+    public InputStream getInputStream(ZipEntry entry) throws IOException {
+        return null;
+    }
+
+    public void close() {
     }
 }
