0b62850e49b5dec6a8e475de9708885539f0689e
Lord-McSweeney <batuharmanci@gmail.com>
2026-05-12 18:10:13 -0700

    globals: Improve Properties parsing logic

--- globals/globals/base/java/util/Properties.java
+++ globals/globals/base/java/util/Properties.java
@@ -4,6 +4,9 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class Properties extends Hashtable<Object, Object> {
+    final int PARSE_KEY = 0;
+    final int PARSE_VALUE = 1;
+
     public Properties() {
         super();
     }
@@ -14,7 +17,7 @@ public class Properties extends Hashtable<Object, Object> {
             0 = parsing key
             1 = parsing value
         */
-        int state = 0;
+        int state = PARSE_KEY;
 
         char[] buffer = new char[stream.available()];
         int bufferLength = 0;
@@ -24,12 +27,36 @@ public class Properties extends Hashtable<Object, Object> {
         char next = (char) stream.read();
         while (stream.available() > 0) {
             switch (state) {
-                case 0:
-                    if (next == ':' || next == '=') {
+                case PARSE_KEY:
+                    if (next == '\\') {
+                        // Skip it
+
+                        // Interpret the next character
+                        char literalNext = (char) stream.read();
+
+                        // Newlines are ignored (TODO do we need to check for \r\n?)
+                        if (literalNext == '\n') {
+                            // Skip any literal whitespace right at the start of
+                            // the next line
+                            next = (char) stream.read();
+                            while (isWhitespace(next)) {
+                                next = (char) stream.read();
+                            }
+                            break;
+                        }
+
+                        next = parseEscapedCharacter(literalNext);
+
+                        buffer[bufferLength] = next;
+                        bufferLength ++;
+
+                        next = (char) stream.read();
+                        break;
+                    } else if (next == ':' || next == '=') {
                         currentKey = (new String(buffer, 0, bufferLength)).trim();
                         bufferLength = 0;
 
-                        state = 1;
+                        state = PARSE_VALUE;
 
                         next = (char) stream.read();
                         break;
@@ -56,14 +83,38 @@ public class Properties extends Hashtable<Object, Object> {
                     next = (char) stream.read();
                     break;
 
-                case 1:
-                    if (next == '\n') {
+                case PARSE_VALUE:
+                    if (next == '\\') {
+                        // Skip it
+
+                        // Interpret the next character
+                        char literalNext = (char) stream.read();
+
+                        // Newlines are ignored (TODO do we need to check for \r\n?)
+                        if (literalNext == '\n') {
+                            // Skip any literal whitespace right at the start of
+                            // the next line
+                            next = (char) stream.read();
+                            while (isWhitespace(next)) {
+                                next = (char) stream.read();
+                            }
+                            break;
+                        }
+
+                        next = parseEscapedCharacter(literalNext);
+
+                        buffer[bufferLength] = next;
+                        bufferLength ++;
+
+                        next = (char) stream.read();
+                        break;
+                    } else if (next == '\n') {
                         String value = new String(buffer, 0, bufferLength);
                         bufferLength = 0;
 
                         super.put(currentKey, value);
 
-                        state = 0;
+                        state = PARSE_KEY;
 
                         next = (char) stream.read();
                         break;
@@ -79,7 +130,7 @@ public class Properties extends Hashtable<Object, Object> {
 
                         super.put(currentKey, value);
 
-                        state = 0;
+                        state = PARSE_KEY;
                         break;
                     }
 
@@ -91,7 +142,7 @@ public class Properties extends Hashtable<Object, Object> {
             }
         }
 
-        if (state == 1) {
+        if (state == PARSE_VALUE) {
             // We haven't processed the item in `next` because the loop ended:
             // do it now
             buffer[bufferLength] = next;
@@ -101,6 +152,23 @@ public class Properties extends Hashtable<Object, Object> {
 
             super.put(currentKey, value);
         }
+    }
+
+    private static char parseEscapedCharacter(char c) {
+        if (c == 'r') {
+            return '\r';
+        } else if (c == 'n') {
+            return '\n';
+        } else if (c == 't') {
+            return '\t';
+        } else {
+            // TODO more
+            return c;
+        }
+    }
+
+    private static boolean isWhitespace(char c) {
+        return c == ' ' || c == '\t' || c == '\r' || c == '\n';
     }
 
     public String getProperty(String key) {
