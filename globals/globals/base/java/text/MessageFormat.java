637a58c533c936de30fc92b2d0d479175ca4e867
Lord-McSweeney <batuharmanci@gmail.com>
2026-05-13 20:34:31 -0700

    globals: Partially implement MessageFormat.format

--- globals/globals/base/java/text/MessageFormat.java
+++ globals/globals/base/java/text/MessageFormat.java
@@ -2,7 +2,23 @@ package java.text;
 
 public class MessageFormat extends Format {
     public static String format(String pattern, Object... arguments) {
-        // TODO implement
-        return pattern;
+        // TODO implement properly- this is good enough for now
+        String result = pattern;
+
+        for (int i = 0; i < arguments.length; i ++) {
+            String argument = String.valueOf(arguments[i]);
+            String search = "{" + i + '}';
+            String[] split = result.split(search);
+
+            StringBuilder combined = new StringBuilder();
+            for (int j = 0; j < split.length; j ++) {
+                combined.append(split[j]);
+                if (j != split.length - 1) {
+                    combined.append(argument);
+                }
+            }
+            result = combined.toString();
+        }
+        return result;
     }
 }
